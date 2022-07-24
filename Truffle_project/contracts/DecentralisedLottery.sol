2719341eab09f027ebc92983afcc59beba9e0be6
mmarton12 <moro.marton@edu.bme.hu>
2022-07-24 23:27:48 +0200

    Implemented finalize function, added tests testing play and finalize (winner is not random)

--- Truffle_project/contracts/DecentralisedLottery.sol
+++ Truffle_project/contracts/DecentralisedLottery.sol
@@ -11,10 +11,17 @@ contract DecentralisedLottery is Ownable{
         uint number;
         uint betSize;
         bool isPlaying;
+        bool hasBeenPaid;
     }
 
-    playerStruct [][18] public playersArray;
-    mapping(address => playerStruct) public playersMap;
+    // Struct storing players (index is used to store number)
+    playerStruct [][18] private playersArray;
+    // Map for storing addresses and lottery entries
+    mapping(address => playerStruct) private playersMap;
+    // Sum of bets for the numbers
+    uint [18] betsForNumbers;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     function play(uint guess) public payable {
 
@@ -30,10 +37,13 @@ contract DecentralisedLottery is Ownable{
 
         // Rest (30% of tx value) goes to the owner as a fee. Could be paid after finalization
         payable(owner()).transfer(msg.value-bet);
+        emit Transfer(msg.sender, owner(), msg.value-bet);
 
         // Store player and bet
-        playersMap[msg.sender] = playerStruct(msg.sender, guess, bet, true);
-        playersArray[guess-2].push(playerStruct(msg.sender, guess, bet, true)); // guess-2 because array indexing starts at 0 but playable numbers start at 2
+        playersMap[msg.sender] = playerStruct(msg.sender, guess, bet, true, false);
+        // guess-2 because array indexing starts at 0 but playable numbers start at 2
+        playersArray[guess-2].push(playerStruct(msg.sender, guess, bet, true, false)); 
+        betsForNumbers[guess-2] += bet;
     }
 
     function getPlayerAddressFromArray(uint index0, uint index1) public view returns(address){
@@ -42,5 +52,42 @@ contract DecentralisedLottery is Ownable{
 
     function getPlayerNumberFromArray(uint index0, uint index1) public view returns(uint){
         return playersArray[index0][index1].number;
+    }
+
+    function finalize() public onlyOwner{
+        //TODO: generate random number
+        uint winningNumber = 7;
+        uint i;
+
+        // Paying the winners
+        for(i = 0; i<playersArray[winningNumber].length; i++){
+            address payable winnerAddr = payable(playersArray[winningNumber][i].playerAddress);
+            uint playerBet = playersArray[winningNumber][i].betSize;
+            uint amountWon = playerBet * jackpot / betsForNumbers[winningNumber];       // Jackpot gets distributed proportionally to the bets
+            
+            if(playersMap[winnerAddr].hasBeenPaid == false){
+                playersMap[winnerAddr].hasBeenPaid = true;
+                winnerAddr.transfer(amountWon);
+                emit Transfer(address(this), winnerAddr, amountWon);
+            }
+        }
+
+        // No winners (returns)
+        if(i == 0){
+            for(i = 0; i < 18; i++){
+                for(uint j = 0; j < playersArray[i].length; j++){
+                    address payable addr = payable(playersArray[i][j].playerAddress);
+                    uint playerBet = playersArray[i][j].betSize;
+
+                    if(playersMap[addr].hasBeenPaid == false){
+                        playersMap[addr].hasBeenPaid = true;
+                        addr.transfer(playerBet);
+                        emit Transfer(address(this), addr, playerBet);
+                    }
+                }
+            }
+        }
+
+        //TODO: restart lottery
     }
 }
--- Truffle_project/test/TestDecentralisedLottery.test.js
+++ Truffle_project/test/TestDecentralisedLottery.test.js
@@ -1,37 +1,63 @@
 const DecentralisedLottery = artifacts.require('DecentralisedLottery');
 const assert = require('assert');
+const toBN = web3.utils.toBN;
 
 
 contract("DecentralisedLottery", (accounts) => {
     let decentralisedLottery;
-    let expectedPlayerAddress;
-    let expectedPlayersNumber;
 
     before(async () => {
         decentralisedLottery = await DecentralisedLottery.deployed();
     });
 
-    describe("after playing the player info should be stored", async () => {
+    describe("after playing the player info should be stored and the fee should be transfered", async () => {
+        let expectedPlayerAddress;
+        let expectedPlayersNumber;
+        let playResult;
+        let playValue;
+
         before("playing using accounts[0] with address 0x777925B7e586F0De1C2312ac9bcd559CBac3eFB0", async () => {
-            await decentralisedLottery.play(8, {from: accounts[0], value: 1e16});
+            playValue = 1e16;
+            playResult = await decentralisedLottery.play(8, {from: accounts[0], value: playValue});
             expectedPlayerAddress = accounts[0]
-/*             const stuff = await decentralisedLottery.getPlayerAddressFromArray(6,0);
-            console.log("resuktingplayeraddress: ", stuff);
-            console.log("Logging: ", expectedPlayerAddress);
-            console.log("playeraddresstostring: ", toString(expectedPlayerAddress)); */
             expectedPlayersNumber = '8';
         });
 
         it("can fetch the player's address", async () => {
             const resultingPlayerAddress = await decentralisedLottery.getPlayerAddressFromArray(6,0);
-            console.log(resultingPlayerAddress);
             assert.equal(toString(resultingPlayerAddress), toString(expectedPlayerAddress), "The addresses should match");
         });
 
         it("can fetch the player's number", async () => {
             const playersNumber = await decentralisedLottery.getPlayerNumberFromArray(6,0);
-            console.log(playersNumber);
             assert.equal(playersNumber, expectedPlayersNumber, "The numbers should match");
-        })
+        });
+
+        it("transfers 30% of the bet to the owner", async () => {
+            assert.equal(playResult.logs[0].args._value.toString(), (playValue*0.3).toString(), "The transferred amount should be the 30% of the bet")
+            assert.equal(playResult.logs[0].args._from, expectedPlayerAddress, "The sender should be the player");
+            assert.equal(playResult.logs[0].args._to, accounts[0], "The recipient should be the owner");
+        });
     });
+
+    describe.only("after finalization the winners should be paid proportionally to their bets", async () => {
+        before("playing with players and then finalize with owner", async () => {
+            await decentralisedLottery.play(8, {from: accounts[5], value: web3.utils.toWei('1', 'ether')});
+            await decentralisedLottery.play(9, {from: accounts[6], value: web3.utils.toWei('1', 'ether')});
+            await decentralisedLottery.play(9, {from: accounts[7], value: web3.utils.toWei('1', 'ether')});
+
+            finalizeResult = await decentralisedLottery.finalize({from: accounts[0]});
+        });
+
+        it("should pay accounts 6 and 7 1.05 eth ((1+1+1)*70%/2)", async () => {
+            assert.equal(finalizeResult.logs[0].args._from, decentralisedLottery.address, "The contract should pay the winners");
+            assert.equal(finalizeResult.logs[0].args._to, accounts[6], "Account 6 should be paid")
+            assert.equal(finalizeResult.logs[0].args._value.toString(), web3.utils.toWei('1.05', 'ether'), "Account 6 should be paid 1.05 eth");
+
+            assert.equal(finalizeResult.logs[1].args._from, decentralisedLottery.address, "The contract should pay the winners");
+            assert.equal(finalizeResult.logs[1].args._to, accounts[7], "Account 7 should be paid")
+            assert.equal(finalizeResult.logs[1].args._value.toString(), web3.utils.toWei('1.05', 'ether'), "Account 7 should be paid 1.05 eth");
+
+        })
+    })
 });
