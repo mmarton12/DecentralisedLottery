24a2835b34679ce1234cad64fa5e4e2386058bec
mmarton12 <moro.marton@edu.bme.hu>
2022-07-25 21:18:35 +0200

    Commenting

--- Truffle_project/contracts/DecentralisedLottery.sol
+++ Truffle_project/contracts/DecentralisedLottery.sol
@@ -12,31 +12,20 @@ contract DecentralisedLottery is Ownable, VRFConsumerBaseV2 {
     
   VRFCoordinatorV2Interface COORDINATOR;
 
-  // Your subscription ID.
+  // subscription ID.
   uint64 s_subscriptionId;
 
-  // Rinkeby coordinator. For other networks,
-  // see https://docs.chain.link/docs/vrf-contracts/#configurations
+  // Rinkeby coordinator.
   address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
 
   // The gas lane to use, which specifies the maximum gas price to bump to.
-  // For a list of available gas lanes on each network,
-  // see https://docs.chain.link/docs/vrf-contracts/#configurations
   bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
-  // Depends on the number of requested values that you want sent to the
-  // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-  // so 100,000 is a safe default for this example contract. Test and adjust
-  // this limit based on the network that you select, the size of the request,
-  // and the processing of the callback request in the fulfillRandomWords()
-  // function.
   uint32 callbackGasLimit = 100000;
 
-  // The default is 3, but you can set this higher.
   uint16 requestConfirmations = 3;
 
-  // For this example, retrieve 2 random values in one request.
-  // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+  // Retrieve 1 random value in 1 request
   uint32 numWords =  1;
 
   uint256 s_randomWords;
@@ -49,7 +38,7 @@ contract DecentralisedLottery is Ownable, VRFConsumerBaseV2 {
     s_subscriptionId = subscriptionId;
   }
 
-  // Assumes the subscription is funded sufficiently.
+  // Requesting random number using chainlink vrf
   function requestRandomWords() private {
     // Will revert if subscription is not set and funded.
     s_requestId = COORDINATOR.requestRandomWords(
@@ -61,6 +50,7 @@ contract DecentralisedLottery is Ownable, VRFConsumerBaseV2 {
     );
   }
   
+  // Getting random number from chainlink vrf
   function fulfillRandomWords(
     uint256, /* requestId */
     uint256[] memory randomWords
